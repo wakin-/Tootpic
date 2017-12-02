b7d68b62c90107c6502ad82652a83e584648c992
wakin <8th.mail@gmail.com>
2017-12-02 10:28:26 +0900

    不要なfunctionを削除

--- functions/index.js
+++ functions/index.js
@@ -10,23 +10,3 @@ exports.nicoScript = functions.https.onRequest((request, response) => {
     response.setHeader('Content-Type', 'text/html');
     response.send(script);
 });
-
-exports.scScript = functions.https.onRequest((req, res) => {
-    var url = "http://soundcloud.com/oembed?format=json&url="+req.query.url+"&maxheight=81";
-    console.log(url);
-    cors(req, res, () => {
-        request({
-            url: url,
-        }, function (error, response, body) {
-            console.log(error);
-            console.log(response);
-            console.log(body);
-            var html = "";
-            if (!error && response.statusCode == 200) {
-                html = '<html><head></head><body>'+JSON.parse(body)+'</body>';
-            }
-            res.setHeader('Content-Type', 'text/html');
-            res.send(html);
-        });
-    });
-});
